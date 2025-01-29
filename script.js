722444f5e4e0110ebd341c497423085f02f1ee51
Anajara <doc.anajara@gmail.com>
2025-01-29 16:51:47 -0300

    Ajustar finais de linha

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btnConsulta = document.querySelector("#btn");
 const listaCidades = document.querySelector("#cidades");
 
 btnConsulta.addEventListener("click", () => {
-    const uf = inputUF.value.trim().toUpperCase(); // Obtém o valor e converte para maiúsculas
+    const uf = inputUF.value.trim().toUpperCase(); 
 
     if (uf.length !== 2) {
         alert("Digite uma UF válida com 2 letras!");
@@ -13,7 +13,7 @@ btnConsulta.addEventListener("click", () => {
     fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
         .then(response => response.json())
         .then(data => {
-            listaCidades.innerHTML = ""; // Limpa a lista antes de adicionar novas cidades
+            listaCidades.innerHTML = "";
             data.forEach(cidade => {
                 const li = document.createElement("li");
                 li.textContent = cidade.nome;
@@ -22,6 +22,6 @@ btnConsulta.addEventListener("click", () => {
         })
         .catch(error => {
             console.error("Erro ao buscar cidades:", error);
-            alert("Erro ao buscar cidades. Verifique a UF e tente novamente.");
+            alert(" Verifique a UF e tente denovo.");
         });
 });
